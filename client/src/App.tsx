df03eb0adc55400df01e29e64d3c9cc228b1b2cd
iberis2 <snow9402@gmail.com>
2023-07-04 10:03:37 +0900

    Feat: App 컴포넌츠에 Router 연결 (#12)
    - createBrowserRouter 에 routerData를 map돌려서 router 생성

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,15 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { routerData } from './router/routerData'
+
+const router = createBrowserRouter(
+  routerData.map(routerElement => ({
+    path: routerElement.path,
+    element: routerElement.element,
+  }))
+)
+
 function App() {
-  return <>app</>
+  return <RouterProvider router={router} />
 }
 
 export default App
