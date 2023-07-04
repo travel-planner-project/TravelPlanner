c469c62c29667d3411f08285549efae25aa9dbf5
iberis2 <snow9402@gmail.com>
2023-07-04 09:57:56 +0900

    Feat: navBar에 들어갈 router data 배열 완성 (#12)
    - lazy 로딩 적용
    - endpoint 임의로 작성(백엔드 api로 수정 필요)
    - /feed, /board 는 home 컴포넌츠로 경로설정, onNavBar: false로 안보이게 해둠

---
+++ client/src/router/routerData.tsx
@@ -0,0 +1,95 @@
+import { Suspense, lazy } from 'react'
+
+const Home = lazy(() => import('../pages/Home'))
+const Planner = lazy(() => import('../pages/Planner'))
+const Profile = lazy(() => import('../pages/Profile'))
+const SignIn = lazy(() => import('../pages/SignIn'))
+
+const Feed = lazy(() => import('../pages/Home'))
+const Board = lazy(() => import('../pages/Home'))
+
+type RouterElement = {
+  id: number
+  path: string
+  label: string
+  element: React.ReactNode
+  onNavBar: boolean
+  withAuth: boolean
+}
+
+export const routerData: RouterElement[] = [
+  {
+    id: 1,
+    path: '/',
+    label: 'HOME',
+    element: (
+      <Suspense fallback={<div>loading</div>}>
+        <Home />
+      </Suspense>
+    ),
+    onNavBar: true,
+    withAuth: false,
+  },
+  {
+    id: 2,
+    path: '/planner',
+    label: 'PLANNER',
+    element: (
+      <Suspense fallback={<div>loading</div>}>
+        <Planner />
+      </Suspense>
+    ),
+    onNavBar: true,
+    withAuth: true,
+  },
+  {
+    id: 3,
+    path: '/profile',
+    label: 'user name',
+    element: (
+      <Suspense fallback={<div>loading</div>}>
+        <Profile />
+      </Suspense>
+    ),
+    onNavBar: true,
+    withAuth: true,
+  },
+  {
+    id: 4,
+    path: '/user/login',
+    label: '로그인',
+    element: (
+      <Suspense fallback={<div>loading</div>}>
+        <SignIn />
+      </Suspense>
+    ),
+    onNavBar: true,
+    withAuth: false,
+  },
+  {
+    id: 5,
+    path: '/feed',
+    label: 'FEED',
+    element: (
+      <Suspense fallback={<div>loading</div>}>
+        <Board />
+      </Suspense>
+    ),
+    onNavBar: false,
+    withAuth: true,
+  },
+  {
+    id: 6,
+    path: '/board',
+    label: 'BOARD',
+    element: (
+      <Suspense fallback={<div>loading</div>}>
+        <Feed />
+      </Suspense>
+    ),
+    onNavBar: false,
+    withAuth: false,
+  },
+]
+
+export const NavBarContent: RouterElement[] = routerData.filter(router => router.onNavBar)
