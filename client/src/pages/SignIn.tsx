72e208c1f45e1d6209928f5632f268622317e019
iberis2 <snow9402@gmail.com>
2023-07-05 20:31:03 +0900

    Feat: 회원가입 틀 완성 (#29)
    - gray200 색상 추가
    - 이메일, 닉네임, 비밀번호 검증 로직 미완성

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -1,5 +1,41 @@
+import styles from './SignIn.module.scss'
+
 function SignIn() {
-  return <div>SignIn</div>
+  return (
+    <form className={styles.container}>
+      <label className={styles.label} htmlFor='email'>
+        이메일
+        <input id='email' type='email' placeholder='이메일을 입력해주세요' required />
+      </label>
+
+      <label className={styles.label} htmlFor='nickName'>
+        닉네임
+        <input id='nickName' type='text' placeholder='사용할 닉네임을 입력해주세요' required />
+      </label>
+
+      <label className={styles.label} htmlFor='password'>
+        비밀번호
+        <input
+          id='password'
+          type='password'
+          placeholder='8~15자 이하 영문, 숫자, 특수문자 조합'
+          required
+        />
+      </label>
+
+      <label className={styles.label} htmlFor='password'>
+        비밀번호 확인
+        <input
+          id='password'
+          type='password'
+          placeholder='비밀번호를 한번 더 입력해주세요'
+          required
+        />
+      </label>
+
+      <button type='submit'>회원가입</button>
+    </form>
+  )
 }
 
 export default SignIn
