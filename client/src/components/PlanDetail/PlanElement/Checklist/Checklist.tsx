adce135e920372065c1409def9157d0a15dda562
iberis2 <snow9402@gmail.com>
2023-07-09 04:25:04 +0900

    Feat: 항목 추가하기 구현 (#40)
    - list 배열의 길이만큼.map()으로 list item 컴포넌트 리턴
    - 버튼 클릭시마다 list 배열의 가장 마지막 요소 +1한 값이 추가됨

--- client/src/components/PlanDetail/PlanElement/Checklist/Checklist.tsx
+++ client/src/components/PlanDetail/PlanElement/Checklist/Checklist.tsx
@@ -1,11 +1,23 @@
+import { useState } from 'react'
 import styles from './Checklist.module.scss'
 import ListItem from './ListItem'
 
 function Checklist() {
+  const [list, setList] = useState<number[]>([1])
+
+  const addList = () => {
+    setList(pre => [...pre, pre[pre.length - 1] + 1])
+  }
+
   return (
     <div className={styles.container}>
       <ul className={styles.checklist}>
-        <ListItem />
+        {list.map(line => (
+          <ListItem key={line} />
+        ))}
+        <button className={styles.addBtn} type='button' onClick={addList}>
+          항목 추가하기
+        </button>
       </ul>
     </div>
   )
