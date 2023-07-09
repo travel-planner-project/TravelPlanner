82ce40d0293d26401f494e8c8deff488a0f33197
sieunnnn <wldsmtldsm65@gmail.com>
2023-07-09 23:10:26 +0900

    Setting: Jwt 설정 파일 / 방식 변경 (#26)

--- server/src/main/java/server/global/security/jwt/JwtAuthenticationFilter.java
+++ server/src/main/java/server/global/security/jwt/JwtAuthenticationFilter.java
@@ -1,38 +1,87 @@
 package server.global.security.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
+import server.global.util.GlobalRequest;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Slf4j
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter extends GenericFilterBean {
-    // 인증 / 인가 작업을 해줌
-    // 토큰을 생성하고 검증하는 컴포넌트 TokenProvider을 이용
-    // 사용자가 로그인 정보로 인증 요청을 보내면 filter가 요청을 받아 인증용 객체 생성
-    // 인증용 객체를 AuthenticationManager 에게 보냄
-    // 인증이 완료된 객체를 SecurityContext에 저장
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private final JwtTokenProvider jwtTokenProvider;
+    private final JwtUtil jwtUtil;
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException{
-        // 헤더에서 JWT 받아옴
-        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
-        // 토큰 유효성 확인
-        if(token != null && jwtTokenProvider.validateToken(token)){
-            // 토큰이 유효하면 토큰으로부터 유저 정보를 받아옴
-            Authentication authentication = jwtTokenProvider.getAuthentication(token);
-            // SecurityContext에 Authentication 객체를 저장함
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+    // HTTP 요청이 오면 WAS(tomcat)가 HttpServletRequest, HttpServletResponse 객체를 만들어 줍니다.
+    // 만든 인자 값을 받아옵니다.
+    // 요청이 들어오면 diFilterInternal 이 딱 한번 실행된다.
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // WebSecurityConfig 에서 보았던 UsernamePasswordAuthenticationFilter 보다 먼저 동작을 하게 됩니다.
+
+        // Access / Refresh 헤더에서 토큰을 가져옴.
+        String accessToken = jwtUtil.getHeaderToken(request, "Access");
+        String refreshToken = jwtUtil.getHeaderToken(request, "Refresh");
+
+        if(accessToken != null) {
+            // 어세스 토큰값이 유효하다면 setAuthentication를 통해
+            // security context에 인증 정보저장
+            if(jwtUtil.tokenValidation(accessToken)){
+                setAuthentication(jwtUtil.getEmailFromToken(accessToken));
+            }
+            // 어세스 토큰이 만료된 상황 && 리프레시 토큰 또한 존재하는 상황
+            else if (refreshToken != null) {
+                // 리프레시 토큰 검증 && 리프레시 토큰 DB에서  토큰 존재유무 확인
+                boolean isRefreshToken = jwtUtil.refreshTokenValidation(refreshToken);
+                // 리프레시 토큰이 유효하고 리프레시 토큰이 DB와 비교했을때 똑같다면
+                if (isRefreshToken) {
+                    // 리프레시 토큰으로 아이디 정보 가져오기
+                    String loginId = jwtUtil.getEmailFromToken(refreshToken);
+                    // 새로운 어세스 토큰 발급
+                    String newAccessToken = jwtUtil.createToken(loginId, "Access");
+                    // 헤더에 어세스 토큰 추가
+                    jwtUtil.setHeaderAccessToken(response, newAccessToken);
+                    // Security context에 인증 정보 넣기
+                    setAuthentication(jwtUtil.getEmailFromToken(newAccessToken));
+                }
+                // 리프레시 토큰이 만료 || 리프레시 토큰이 DB와 비교했을때 똑같지 않다면
+                else {
+                    jwtExceptionHandler(response, "RefreshToken Expired", HttpStatus.BAD_REQUEST);
+                    return;
+                }
+            }
         }
-        chain.doFilter(request, response);
+
+        filterChain.doFilter(request,response);
+    }
+
+    // SecurityContext 에 Authentication 객체를 저장합니다.
+    public void setAuthentication(String email) {
+        Authentication authentication = jwtUtil.createAuthentication(email);
+        // security가 만들어주는 securityContextHolder 그 안에 authentication을 넣어줍니다.
+        // security가 securitycontextholder에서 인증 객체를 확인하는데
+        // jwtAuthfilter에서 authentication을 넣어주면 UsernamePasswordAuthenticationFilter 내부에서 인증이 된 것을 확인하고 추가적인 작업을 진행하지 않습니다.
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    // Jwt 예외처리
+    public void jwtExceptionHandler(HttpServletResponse response, String msg, HttpStatus status) {
+        response.setStatus(status.value());
+        response.setContentType("application/json");
+        try {
+            String json = new ObjectMapper().writeValueAsString(new GlobalRequest(msg, status.value()));
+            response.getWriter().write(json);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
     }
 }
---
+++ server/src/main/java/server/global/security/jwt/RefreshToken.java
@@ -0,0 +1,38 @@
+package server.global.security.jwt;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String refreshToken;
+
+    @NotBlank
+    private String email;
+
+    public RefreshToken(String token, String email) {
+
+        this.refreshToken = token;
+        this.email = email;
+    }
+
+    public RefreshToken updateToken(String token) {
+
+        this.refreshToken = token;
+        return this;
+    }
+}
