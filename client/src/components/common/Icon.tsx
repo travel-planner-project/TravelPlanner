dfbd35244062f0d93dfdb3c1bd5ba545e3cccc02
iberis2 <snow9402@gmail.com>
2023-07-09 13:55:29 +0900

    Design: Icon size 10, 12 추가 (#40)
    - git conflict 방지용

--- client/src/components/common/Icon.tsx
+++ client/src/components/common/Icon.tsx
@@ -30,7 +30,7 @@ type IconNameType =
 
 type IconProps = {
   name: IconNameType
-  size?: 16 | 18 | 20 | 24 | 28 | 42 | 50 | 64 | 70
+  size?: 10 | 12 | 16 | 18 | 20 | 24 | 28 | 42 | 50 | 70
 }
 
 function Icon({ name, size = 24 }: IconProps) {
