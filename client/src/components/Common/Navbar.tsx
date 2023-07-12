6f936788d5e275a39e10e35dbe1490da6ad14e58
iberis2 <snow9402@gmail.com>
2023-07-12 13:29:20 +0900

    Feat: logout 버튼 UI 추가 (#51)

--- client/src/components/Common/Navbar.tsx
+++ client/src/components/Common/Navbar.tsx
@@ -2,6 +2,9 @@ import { NavLink } from 'react-router-dom'
 import { NavBarContent } from '../../router/routerData'
 import styles from './Navbar.module.scss'
 import NavbarProfile from './NavbarProfile'
+import Icon from './Icon'
+
+const ISLOGIN = true
 
 function Navbar() {
   return (
@@ -22,6 +25,12 @@ function Navbar() {
           )
         })}
       </ul>
+      {ISLOGIN && (
+        <button type='button' className={styles.logout}>
+          <Icon name='box-arrow-left' size={24} />
+          <span>logout</span>
+        </button>
+      )}
     </div>
   )
 }
