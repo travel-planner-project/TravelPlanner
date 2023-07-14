fab5d7cf57a0aa3f2165268d7f23d6f019564c31
iberis2 <snow9402@gmail.com>
2023-07-14 10:51:50 +0900

    Feat: 세션Storage를 위해 window !== undefined 검증 추가(#51)
    -  window !== undefined 일 때만 세션 스토리지에 Recoil Atom 의 유저 정보 저장하도록 if문 추가

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -13,21 +13,25 @@ type AtomEffectParameterType<T> = {
   onSet: (callBack: (newValue: T, oldValue: T | DefaultValue, isReset: boolean) => void) => void
 }
 
+const isBrowser = () => typeof window !== 'undefined'
+
 const sessionStorageEffect =
   (key: string) =>
   ({ setSelf, onSet }: AtomEffectParameterType<any>) => {
-    const savedValue = sessionStorage.getItem(key)
-    if (savedValue !== null) {
-      setSelf(JSON.parse(savedValue))
-    }
-
-    onSet((newValue, _, isReset) => {
-      if (isReset) {
-        sessionStorage.removeItem(key)
-      } else {
-        sessionStorage.setItem(key, JSON.stringify(newValue))
+    if (isBrowser()) {
+      const savedValue = sessionStorage.getItem(key)
+      if (savedValue !== null) {
+        setSelf(JSON.parse(savedValue))
       }
-    })
+
+      onSet((newValue, _, isReset) => {
+        if (isReset) {
+          sessionStorage.removeItem(key)
+        } else {
+          sessionStorage.setItem(key, JSON.stringify(newValue))
+        }
+      })
+    }
   }
 
 // eslint-disable-next-line import/prefer-default-export
