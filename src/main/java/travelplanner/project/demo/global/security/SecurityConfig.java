7345dfb0d6081f56c0ab05d2f794695320b1a9d5
sieunnnn <wldsmtldsm65@gmail.com>
2023-07-19 03:29:37 +0900

    Setting: 기본 설정

---
+++ src/main/java/travelplanner/project/demo/global/security/SecurityConfig.java
@@ -0,0 +1,45 @@
+package travelplanner.project.demo.global.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .cors().and()
+                .authorizeHttpRequests()
+                    .requestMatchers("/**").permitAll();
+
+        return http.build();
+    }
+
+    // CORS
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        ((UrlBasedCorsConfigurationSource) source).registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
