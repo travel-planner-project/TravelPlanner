a0ae0d301c81500028276afe12cb02baafe62680
sieunnnn <wldsmtldsm65@gmail.com>
2023-07-24 00:27:07 +0900

    Test: 스웨거 테스트 코드 작성 / 포스트맨 테스트 (완) (#79)

--- src/main/java/travelplanner/project/demo/member/profile/dto/request/PasswordUpdateRequest.java
+++ src/main/java/travelplanner/project/demo/member/profile/dto/request/PasswordUpdateRequest.java
@@ -1,5 +1,6 @@
 package travelplanner.project.demo.member.profile.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class PasswordUpdateRequest {
 
+    @Schema(description = "유저 인덱스", example = "1")
     private Long userId;
+
+    @Schema(description = "비밀번호", example = "123456789")
     private String password;
 }
