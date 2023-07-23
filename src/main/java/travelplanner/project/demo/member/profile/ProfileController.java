9b94df74da06fbc7ae978124c9395ed7bd6ee66d
sieunnnn <wldsmtldsm65@gmail.com>
2023-07-24 00:32:43 +0900

    Test: 스웨거 테스트 코드 작성 / 포스트맨 테스트 완료 (#79)

--- src/main/java/travelplanner/project/demo/member/profile/ProfileController.java
+++ src/main/java/travelplanner/project/demo/member/profile/ProfileController.java
@@ -1,9 +1,18 @@
 package travelplanner.project.demo.member.profile;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import travelplanner.project.demo.global.exception.ApiException;
 import travelplanner.project.demo.global.exception.Exception;
 import travelplanner.project.demo.global.exception.ExceptionType;
 import travelplanner.project.demo.member.profile.Service.ProfileService;
@@ -14,6 +23,8 @@ import travelplanner.project.demo.member.profile.dto.response.ProfileResponse;
 
 import java.io.IOException;
 
+
+@Tag(name = "Profile", description = "프로필 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/profile")
@@ -23,33 +34,84 @@ public class ProfileController {
     private final UserService userService;
 
 
-    // 특정 유저의 프로필
+    @Operation(summary = "프로필 상세")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "특정 유저의 프로필 접근 성공",
+                    content = @Content(schema = @Schema(implementation = ProfileResponse.class))),
+            @ApiResponse(responseCode = "403", description = "로그인한 유저와 프로필 유저가 같지 않은 경우",
+                    content = @Content(schema = @Schema(implementation = ApiException.class))),
+            @ApiResponse(responseCode = "404", description = "특정 유저를 찾을 수 없는 경우",
+                    content = @Content(schema = @Schema(implementation = ApiException.class)))
+    })
     @GetMapping("")
-    public ResponseEntity<ProfileResponse> findUserProfile(@RequestParam Long userId) throws Exception {
+    public ResponseEntity<ProfileResponse> findUserProfile(
+            @Parameter(name = "userId", description = "유저 인덱스", in = ParameterIn.QUERY) // swagger
+            @RequestParam Long userId) throws Exception {
         return ResponseEntity.ok(profileService.findUserProfile(userId));
     }
 
-    // 프로필 수정
+
+    @Operation(summary = "프로필 수정")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "특정 유저의 프로필 수정 성공"),
+            @ApiResponse(responseCode = "403", description = "로그인한 유저와 프로필 유저가 같지 않은 경우",
+                    content = @Content(schema = @Schema(implementation = ApiException.class))),
+            @ApiResponse(responseCode = "404", description = "특정 유저를 찾을 수 없는 경우",
+                    content = @Content(schema = @Schema(implementation = ApiException.class)))
+    })
     @PutMapping("")
     public void updateUserProfile(
-            @RequestParam Long userId, @RequestParam MultipartFile imgFile, @RequestParam String userNickname) throws Exception, IOException {
+            @Parameter(name = "userId", description = "유저 인덱스", in = ParameterIn.QUERY)
+            @RequestParam Long userId,
+            @Parameter(name = "imgFile", description = "이미지 파일", in = ParameterIn.QUERY)
+            @RequestParam MultipartFile imgFile,
+            @Parameter(name = "userNickname", description = "유저 닉네임", in = ParameterIn.QUERY)
+            @RequestParam String userNickname) throws Exception, IOException {
 
             profileService.updateUserProfileImg(userId, imgFile, userNickname);
     }
 
-    //  회원 비밀번호 확인
+
+    @Operation(summary = "회원 수정 : 비밀번호 확인")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "비밀번호 일치 확인"),
+            @ApiResponse(responseCode = "400", description = "비밀번호가 일치하지 않는 경우",
+                    content = @Content(schema = @Schema(implementation = ApiException.class))),
+            @ApiResponse(responseCode = "403", description = "로그인한 유저와 프로필 유저가 같지 않은 경우",
+                    content = @Content(schema = @Schema(implementation = ApiException.class))),
+            @ApiResponse(responseCode = "404", description = "특정 유저를 찾을 수 없는 경우",
+                    content = @Content(schema = @Schema(implementation = ApiException.class)))
+    })
     @PostMapping("/user")
     public boolean checkUserPassword(@RequestBody PasswordCheckRequest request) throws Exception {
         return userService.checkUserPassword(request);
     }
 
-    // 회원 비밀번호 변경
+
+    @Operation(summary = "회원 수정 : 비밀번호 변경")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "비밀번호 변경 완료"),
+            @ApiResponse(responseCode = "403", description = "로그인한 유저와 프로필 유저가 같지 않은 경우",
+                    content = @Content(schema = @Schema(implementation = ApiException.class))),
+            @ApiResponse(responseCode = "404", description = "특정 유저를 찾을 수 없는 경우",
+                    content = @Content(schema = @Schema(implementation = ApiException.class)))
+    })
     @PatchMapping("/user")
     public void updateUserPassword(@RequestBody PasswordUpdateRequest request) throws Exception {
         userService.updatePassword(request);
     }
 
-    // 회원 탈퇴
+
+    @Operation(summary = "회원 수정 : 회원 탈퇴")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "비밀번호 변경 완료"),
+            @ApiResponse(responseCode = "400", description = "비밀번호가 일치하지 않는 경우",
+                    content = @Content(schema = @Schema(implementation = ApiException.class))),
+            @ApiResponse(responseCode = "403", description = "로그인한 유저와 프로필 유저가 같지 않은 경우",
+                    content = @Content(schema = @Schema(implementation = ApiException.class))),
+            @ApiResponse(responseCode = "404", description = "특정 유저를 찾을 수 없는 경우",
+                    content = @Content(schema = @Schema(implementation = ApiException.class)))
+    })
     @DeleteMapping("/user")
     public void deleteUser(@RequestBody PasswordCheckRequest request) throws Exception{
 
