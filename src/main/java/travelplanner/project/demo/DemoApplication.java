2c52a71ac281be210f168015404ddb25142ba15e
sieunnnn <wldsmtldsm65@gmail.com>
2023-07-23 23:16:23 +0900

    Setting: S3 를 위한 세팅 파일 변경 (#79)

--- src/main/java/travelplanner/project/demo/DemoApplication.java
+++ src/main/java/travelplanner/project/demo/DemoApplication.java
@@ -2,9 +2,14 @@ package travelplanner.project.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class DemoApplication {
+
+	static {
+		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
--- src/test/java/travelplanner/project/demo/DemoApplicationTests.java
+++ src/test/java/travelplanner/project/demo/DemoApplicationTests.java
@@ -2,8 +2,15 @@ package travelplanner.project.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"cloud.aws.credentials.access-key=test-access-key",
+		"cloud.aws.credentials.secret-key=test-secret-key",
+		"cloud.aws.region.static=test-region",
+		"cloud.aws.s3.bucket=test-bucket"
+})
 class DemoApplicationTests {
 
 	@Test
