138d2a04b16055f4e4fe22c3b5503b37f3869a55
dd-stack <117844745+dd-stack@users.noreply.github.com>
2023-07-27 14:10:08 +0900

    Feat: 비밀번호 확인 컴포넌트 api 연결 (#97)

--- client/src/components/Profile/RenderCheck.tsx
+++ client/src/components/Profile/RenderCheck.tsx
@@ -1,11 +1,16 @@
 import styles from './RenderCheck.module.scss'
 
-function RenderCheck() {
+type RenderCheckProps = {
+  onCurrentPasswordChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+  onCheckedSubmit: (event: React.FormEvent<HTMLFormElement>) => void
+}
+
+function RenderCheck({ onCurrentPasswordChange, onCheckedSubmit }: RenderCheckProps) {
   return (
-    <form className={styles.entireContainer}>
+    <form className={styles.entireContainer} onSubmit={onCheckedSubmit}>
       <label htmlFor='password' className={styles.label}>
         현재 비밀번호
-        <input id='password' type='password' required />
+        <input id='password' type='password' onChange={onCurrentPasswordChange} required />
       </label>
       <button type='submit' className={styles.confirmButton}>
         확인
--- client/src/pages/Profile/EditPassword.tsx
+++ client/src/pages/Profile/EditPassword.tsx
@@ -1,22 +1,43 @@
 import { useState } from 'react'
-import RenderCheckPage from '../../components/Profile/RenderCheck'
+import { useRecoilValue } from 'recoil'
+import { userInfo } from '../../store/store'
+import { CheckPassword } from '../../apis/user'
+import RenderCheck from '../../components/Profile/RenderCheck'
 import RenderEdit from '../../components/Profile/RenderEdit'
-import RenderScuccess from '../../components/Profile/RenderSuccess'
 
 function EditPassword() {
-  const [page, setPage] = useState('check')
+  const { userId } = useRecoilValue(userInfo)
+  const [isChecked, SetIsChecked] = useState(false)
+  const [currentPassword, setCurrentPassword] = useState('')
 
-  // 페이지 상태에 따라 화면 전환
-  switch (page) {
-    case 'check':
-      return <RenderCheckPage />
-    case 'edit':
-      return <RenderEdit />
-    case 'success':
-      return <RenderScuccess />
-    default:
-      return <RenderCheckPage />
+  const onCheckedSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    CheckPassword({ userId: userId, password: currentPassword }).then(response => {
+      if (response?.status === 200) {
+        SetIsChecked(true)
+      }
+      if (response?.status !== 200) {
+        alert('비밀번호가 일치하지 않습니다.')
+      }
+    })
   }
+
+  const onCurrentPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrentPassword(event.target.value)
+  }
+
+  return (
+    <>
+      {isChecked ? (
+        <RenderEdit />
+      ) : (
+        <RenderCheck
+          onCurrentPasswordChange={onCurrentPasswordChange}
+          onCheckedSubmit={onCheckedSubmit}
+        />
+      )}
+    </>
+  )
 }
 
 export default EditPassword
