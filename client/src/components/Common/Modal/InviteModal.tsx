d52084eea287b1992a9f4603580873f01ab97346
snow <120469477+iberis2@users.noreply.github.com>
2023-07-28 12:52:16 +0900

    Feat: 친구 추가용 친구 찾기 모달 UI (#100)
    * Feat: 재사용 가능 Modal UI 완성 (#65)
    
    * Feat: 나눠져있던 ModalContent 를 하나의 컴포넌트로 합침 (#65)
    
    * Feat: 모달을 전역 상태에서 관리 및 커스텀 훅으로 뺌 (#65)
    
    - 재사용성을 높이고 책임 분리를 위해
      - 모달 밖에서는 open modal과 모달에 전달할 데이터만 관리
      - 모달 안에서 close modal 실행
    - recoil 저장소에 modal에 들어갈 데이터들을 관리
    - 모달을 사용하는 부모 컴포넌트에서 useModal의 openModal 로 모달 데이터 전달하면
    - useModal 에서 recoil 저장소로 modal data 업데이트
    - 모달 컴포넌트 안에서 useModal 의 closeModal 함수로 모달 닫기 관리
    
    * Feat: 친구 초대 모달 친구 검색 결과 분기 (#65)
    
    - 검색 버튼을 1번이라도 클릭하면 isSearchBtnDirty state = true 로 변경
    - 친구 검색 결과가 있을 때 → checkbox가 체크된 친구 정보 1건 표시
      - 체크박스 해제 불가
    - 친구 검색 결과 없을  때 → 일치하는 사용자가 없습니다 문구 표시
    
    * Rename: common의 하위 폴더Navbar, Modal 생성 및 파일분리(#65)
    
    - components/common의 파일이 많아져서 Modal / Navbar 폴더 생성하고 각 파일들 폴더 안으로 이동
    
    * Feat: 친구초대, 플래너 추가 모달 분리 (#65)
    
    - 친구 초대, 플래너 추가 모달의 로직이 다르므로 <Modal type='' /> 과 switch문으로  모달 분리
    
    * Feat: 이메일 검색 → 체크박스 체크 → 초대(submit)클릭되도록 변경 (#65)
    
    - friend 객체 state 에 isChecked : boolean 항목을 추가
    - 체크박스의 checked 속성(event.target.checked :boolean)에 따라 isChecked 항목값 변경
    - submit 버튼 클릭시 friend.isChecked = false 이면
    alert('초대하실 친구를 선택 후 초대 버튼을 눌러주세요' ) 알림

---
+++ client/src/components/Common/Modal/InviteModal.tsx
@@ -0,0 +1,100 @@
+import { useState } from 'react'
+import styles from './Modal.module.scss'
+import FriendInfo, { FriendType } from './FriendInfo'
+
+// 가짜 Friend 데이터
+const Friend = {
+  id: 123,
+  profileImg: 'https://i.ytimg.com/vi/y9bWhYGvBlk/maxresdefault.jpg',
+  userNickname: '닉네임',
+  email: 'test123@naver.com',
+}
+
+type InviteModalProp = {
+  onClose: () => void
+  title: string
+  description: string
+  placeholder: string
+  submitButton: string
+  onSubmit: (input: string) => void
+}
+
+function InviteModal({
+  onClose,
+  title,
+  description,
+  placeholder,
+  submitButton,
+  onSubmit,
+}: InviteModalProp) {
+  const [inputValue, setInputValue] = useState('')
+  const [friend, setFriend] = useState<FriendType>({})
+  const [isSearchBtnDirty, setIsSearchBtnDirty] = useState(false)
+
+  const handleSearch = async () => {
+    console.log('input 입력값:', inputValue, '을 서버로 보내고 받은 응답으로 friend 상태 업데이트')
+
+    // 친구 검색 api 에 inputValue state 를 넣어서 request 전송하고
+    // api의 응답 데이터를 setFriend(response.data) 로 업데이트
+
+    // const { status, data } = await searchFriend( inputValue )
+    // if(status === 200){
+    setIsSearchBtnDirty(true)
+    //    setFriend( {...data, isChecked: false } )
+    setFriend({ ...Friend, isChecked: false })
+    // }
+  }
+
+  const handleSubmit = async (event: React.FormEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    if (!isSearchBtnDirty) {
+      return alert('이메일로 친구 검색을 먼저 해주세요')
+    }
+
+    if (!friend.isChecked) {
+      return alert('초대하실 친구를 선택 후 초대 버튼을 눌러주세요')
+    }
+
+    // const { status, data } =  await onSubmit(friend.email)
+    // if(status !== 200) { return alert('친구 초대에 실패했습니다. 잠시 후 다시 시도해주세요.') }
+
+    onClose()
+  }
+
+  return (
+    <>
+      <label htmlFor={title} className={styles.title}>
+        {title}
+      </label>
+      <div className={styles.description}>{description}</div>
+      <div className={styles.inputSearchBox}>
+        <input
+          id={title}
+          className={styles.input}
+          placeholder={placeholder}
+          value={inputValue}
+          onChange={event => setInputValue(event.target.value)}
+        />
+        <button type='button' className={styles.searchBtn} onClick={handleSearch}>
+          검색
+        </button>
+      </div>
+      {isSearchBtnDirty && (
+        <FriendInfo
+          friend={friend}
+          onChecked={(isChecked: boolean) => setFriend({ ...friend, isChecked })}
+        />
+      )}
+      <div className={styles.btnBox}>
+        <button type='submit' className={styles.submitBtn} onClick={handleSubmit}>
+          {submitButton}
+        </button>
+        <button type='button' className={styles.cancelBtn} onClick={onClose}>
+          취소
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default InviteModal
---
+++ client/src/components/Common/Modal/Modal.tsx
@@ -0,0 +1,106 @@
+import { useEffect, useMemo, useState } from 'react'
+import styles from './Modal.module.scss'
+import useModal from '../../../hooks/useModal'
+import InviteModal from './InviteModal'
+
+type ModalContentProp = {
+  onClose: () => void
+  title: string
+  description: string
+  placeholder: string
+  submitButton: string
+  onSubmit: (input: string) => void
+}
+
+function ModalContent({
+  onClose,
+  title,
+  description,
+  placeholder,
+  submitButton,
+  onSubmit,
+}: ModalContentProp) {
+  const [inputValue, setInputValue] = useState('')
+
+  const handleSubmit = async (event: React.FormEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    // const { status, data } =  await onSubmit(inputValue)
+    // if(status !== 200) { alert(data.message) }
+
+    onClose()
+  }
+
+  return (
+    <>
+      <label htmlFor={title} className={styles.title}>
+        {title}
+      </label>
+      <div className={styles.description}>{description}</div>
+      <div className={styles.inputSearchBox}>
+        <input
+          id={title}
+          className={styles.input}
+          placeholder={placeholder}
+          value={inputValue}
+          onChange={event => setInputValue(event.target.value)}
+        />
+      </div>
+      <div className={styles.btnBox}>
+        <button type='submit' className={styles.submitBtn} onClick={handleSubmit}>
+          {submitButton}
+        </button>
+        <button type='button' className={styles.cancelBtn} onClick={onClose}>
+          취소
+        </button>
+      </div>
+    </>
+  )
+}
+
+type ModalProps = {
+  type: 'invite' | 'create-planner'
+}
+
+function Modal({ type }: ModalProps) {
+  const { modalData, closeModal } = useModal()
+
+  const handleEscBtn = (event: KeyboardEvent) => {
+    const key = event.key || event.keyCode
+    if (key === 'Escape' || key === 27) {
+      closeModal()
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleEscBtn)
+    return () => document.removeEventListener('keydown', handleEscBtn)
+  }, [])
+
+  const modalContent = useMemo(() => {
+    switch (type) {
+      case 'invite':
+        return <InviteModal onClose={closeModal} {...modalData} />
+      case 'create-planner':
+        return <ModalContent onClose={closeModal} {...modalData} />
+      default: {
+        return <ModalContent onClose={closeModal} {...modalData} />
+      }
+    }
+  }, [type, modalData])
+
+  return (
+    modalData.isOpen && (
+      <div className={styles.background} onClick={closeModal} role='presentation'>
+        <div
+          className={styles.wrapper}
+          onClick={(e: React.MouseEvent) => e.stopPropagation()}
+          role='presentation'
+        >
+          <form className={styles.content}>{modalContent}</form>
+        </div>
+      </div>
+    )
+  )
+}
+
+export default Modal
