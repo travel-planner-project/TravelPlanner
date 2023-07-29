37d10ec069ad6aa4c03e65725c5374cdaec53c36
sieunnnn <wldsmtldsm65@gmail.com>
2023-07-29 13:21:14 +0900

    Feat: 유저디테일 implements 위치 변경  (#103)

---
+++ src/main/java/travelplanner/project/demo/global/security/PrincipalDetails.java
@@ -0,0 +1,77 @@
+package travelplanner.project.demo.global.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import travelplanner.project.demo.member.Member;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+public class PrincipalDetails implements UserDetails, OAuth2User {
+
+    private Member member;
+
+    // 구글 로그인
+    private Map<String, Object> attributes;
+
+    public PrincipalDetails(Member member, Map<String, Object> attributes) {
+        this.member = member;
+        this.attributes = attributes;
+    }
+
+    // 구글 로그인
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return member.getRole().toString();
+            }
+        });
+
+        return collection;
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
