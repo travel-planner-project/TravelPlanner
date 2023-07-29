6c0ba5928b44607ca3e18187d4d4660410c6f0b8
sieunnnn <wldsmtldsm65@gmail.com>
2023-07-29 13:17:16 +0900

    Setting: Redis 설정 추가 (#103)

---
+++ src/main/java/travelplanner/project/demo/global/config/RedisConfig.java
@@ -0,0 +1,23 @@
+package travelplanner.project.demo.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("server", 6379));
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        return template;
+    }
+}
