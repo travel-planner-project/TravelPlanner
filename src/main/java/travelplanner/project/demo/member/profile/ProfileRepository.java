72369d7f21cc596e89de981c5e2d86456eeed1ae
toychip <dlawnsgud427@naver.com>
2023-08-01 23:04:17 +0900

    Fix: Entity 기본키 전부 id로 변경 - ProfileRepository(#112)

--- src/main/java/travelplanner/project/demo/member/profile/ProfileRepository.java
+++ src/main/java/travelplanner/project/demo/member/profile/ProfileRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
 
-    Profile findProfileByMemberUserId(Long userId);
+    Profile findProfileByMemberId(Long userId);
 }
