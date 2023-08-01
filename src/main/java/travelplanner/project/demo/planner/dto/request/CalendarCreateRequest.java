d86d69088c97ff83f31869de7f762c64cc2f8509
toychip <dlawnsgud427@naver.com>
2023-08-01 22:03:37 +0900

    Fix: PlannerDate -> CalendarCreateRequest(#112)

--- src/main/java/travelplanner/project/demo/planner/dto/request/CalendarCreateRequest.java
+++ src/main/java/travelplanner/project/demo/planner/dto/request/CalendarCreateRequest.java
@@ -3,12 +3,9 @@ package travelplanner.project.demo.planner.dto.request;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
 @Data
 @NoArgsConstructor
-public class DateCreateRequest {
+public class CalendarCreateRequest {
 
     // 사용자가 정한 날짜, Formatter 추가해야함.
     private String eachDate;
