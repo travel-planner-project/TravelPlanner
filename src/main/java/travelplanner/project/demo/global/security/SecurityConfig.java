0a95f4f92d59e40a479cc44ab319759475d3b99b
sieunnnn <wldsmtldsm65@gmail.com>
2023-08-01 14:43:13 +0900

    Test: OPTIONS 메소드 문제인지 확인하기 위해 코드 변경  (#103)

--- src/main/java/travelplanner/project/demo/global/security/SecurityConfig.java
+++ src/main/java/travelplanner/project/demo/global/security/SecurityConfig.java
@@ -1,8 +1,10 @@
 package travelplanner.project.demo.global.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -40,7 +42,9 @@ public class SecurityConfig {
                 .csrf().disable()
                 .cors()
                 .and()
-                .authorizeHttpRequests()
+                .authorizeRequests()
+                    .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                    .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                     .requestMatchers("/swagger-ui/**", "/v3/**").permitAll()
                     .requestMatchers("/auth/**").permitAll()
                 .anyRequest().authenticated()
