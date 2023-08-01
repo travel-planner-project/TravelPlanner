25e9fce0c031533a0523bea098401d69f1329abd
dd-stack <117844745+dd-stack@users.noreply.github.com>
2023-08-02 02:20:08 +0900

    Feat: useParams 커스텀 훅에 추가 (#108)

--- client/src/hooks/useRouter.ts
+++ client/src/hooks/useRouter.ts
@@ -1,10 +1,12 @@
-import { useNavigate, useLocation } from 'react-router-dom'
+import { useNavigate, useLocation, useParams } from 'react-router-dom'
 
 const useRouter = () => {
   const navigate = useNavigate()
   const { pathname } = useLocation()
+  const params = useParams()
 
   return {
+    params,
     pathname,
     routeTo: navigate,
   }
