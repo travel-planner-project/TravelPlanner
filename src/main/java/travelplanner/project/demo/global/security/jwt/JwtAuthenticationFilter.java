44dd3ab61cc884a577da3ad6eb100c49f7b169c9
sieunnnn <wldsmtldsm65@gmail.com>
2023-08-02 12:27:40 +0900

    Fix: 매핑 수정 (#109)

--- src/main/java/travelplanner/project/demo/global/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/travelplanner/project/demo/global/security/jwt/JwtAuthenticationFilter.java
@@ -9,10 +9,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 import travelplanner.project.demo.global.exception.Exception;
 import travelplanner.project.demo.global.exception.ExceptionType;
+import travelplanner.project.demo.global.exception.TokenExpiredException;
 import travelplanner.project.demo.global.util.TokenUtil;
 import travelplanner.project.demo.global.util.CookieUtil;
 
@@ -48,19 +50,18 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         // JWT 토큰 유효성 검사
         try {
             if (!tokenUtil.isValidToken(accessToken)) {
-                // 어세스 토큰이 유효하지 않을 경우(만료된 경우 포함), 401 Unauthorized 오류 반환
-                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid or expired access token.");
-                return;
+                // 어세스 토큰이 유효하지 않을 경우(만료된 경우 포함), 예외를 던짐
+                throw new AuthenticationException("Invalid or expired access token.") {};
             }
         } catch (io.jsonwebtoken.ExpiredJwtException e) {
-            // 토큰이 만료된 경우, 401 Unauthorized 오류 반환
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token has expired.");
-            return;
+            // 토큰이 만료된 경우, 예외를 던짐
+            throw new TokenExpiredException();
+
         } catch (Exception e) {
-            // 토큰 검증 과정에서 예외가 발생하면, 401 Unauthorized 오류 반환
-            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Error verifying token.");
-            return;
+            // 토큰 검증 과정에서 예외가 발생하면, 예외를 던짐
+            throw new AuthenticationException("Error verifying token.") {};
         }
+
 
         String principal = tokenUtil.getEmail(accessToken);
 
