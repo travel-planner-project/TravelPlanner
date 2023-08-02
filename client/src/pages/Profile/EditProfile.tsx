30d8ba04c51646b02f7fa651f8898d996bc9a07e
dd-stack <117844745+dd-stack@users.noreply.github.com>
2023-08-02 13:50:57 +0900

    Refactor: VAC 패턴으로 정리 (#108)

--- client/src/pages/Profile/EditProfile.tsx
+++ client/src/pages/Profile/EditProfile.tsx
@@ -6,6 +6,78 @@ import { editProfile } from '../../apis/user'
 import styles from './EditProfile.module.scss'
 import Icon from '../../components/Common/Icon'
 
+type EditProfileViewProps = {
+  routeTo: any
+  userNickname: string
+  editNickname: string
+  previewImage: string
+  handleImageChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+  handleDeleteImage: () => void
+  handleNicknameChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+  handleKeyDown: (event: React.KeyboardEvent<HTMLInputElement>) => void
+  handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void
+}
+
+function EditProfileView({
+  routeTo,
+  userNickname,
+  editNickname,
+  previewImage,
+  handleImageChange,
+  handleDeleteImage,
+  handleNicknameChange,
+  handleKeyDown,
+  handleSubmit,
+}: EditProfileViewProps) {
+  return (
+    <form className={styles.entireContainer} onSubmit={handleSubmit}>
+      <div className={styles.profileContainer}>
+        <div className={styles.profileBox}>
+          {previewImage ? (
+            <>
+              <img src={previewImage} alt='profile' />
+              <button className={styles.deleteImage} type='button' onClick={handleDeleteImage}>
+                <Icon name='x-circle-blue' size={24} />
+              </button>
+            </>
+          ) : (
+            <Icon name='profile' size={64} />
+          )}
+        </div>
+        <label className={styles.cameraBox} htmlFor='imageInput'>
+          <Icon name='camera' size={24} />
+        </label>
+        <input
+          type='file'
+          id='imageInput'
+          accept='image/*'
+          onChange={handleImageChange}
+          style={{ display: 'none' }}
+        />
+      </div>
+      <input
+        className={styles.nickNameInput}
+        type='text'
+        maxLength={16}
+        placeholder={userNickname}
+        value={editNickname}
+        onChange={handleNicknameChange}
+        onKeyDown={handleKeyDown}
+        required
+      />
+      <p className={styles.guideText}>닉네임은 공백 없이 16자 이하로 입력 가능합니다.</p>
+      <div className={styles.buttonsWrapper}>
+        <button className={styles.blueButton} type='submit'>
+          확인
+        </button>
+        <button className={styles.button} type='button' onClick={() => routeTo(-1)}>
+          취소
+        </button>
+      </div>
+    </form>
+  )
+}
+
 function EditProfile() {
   const { routeTo } = useRouter()
   const { userId, email, userNickname, profileImgUrl } = useRecoilValue(userInfo)
@@ -29,7 +101,6 @@ function EditProfile() {
     // 선택된 파일이 있는 경우
     if (event.target.files) {
       setSelectedImage(event.target.files[0])
-      console.log(typeof event.target.files[0])
       updatePreviewImage(event.target.files[0])
     }
     // 그냥 취소를 누르면 아무일도 일어나지 않는다.
@@ -96,53 +167,19 @@ function EditProfile() {
     })
   }
 
-  return (
-    <form className={styles.entireContainer} onSubmit={handleSubmit}>
-      <div className={styles.profileContainer}>
-        <div className={styles.profileBox}>
-          {previewImage ? (
-            <>
-              <img src={previewImage} alt='profile' />
-              <button className={styles.deleteImage} type='button' onClick={handleDeleteImage}>
-                <Icon name='x-circle-blue' size={24} />
-              </button>
-            </>
-          ) : (
-            <Icon name='profile' size={64} />
-          )}
-        </div>
-        <label className={styles.cameraBox} htmlFor='imageInput'>
-          <Icon name='camera' size={24} />
-        </label>
-        <input
-          type='file'
-          id='imageInput'
-          accept='image/*'
-          onChange={handleImageChange}
-          style={{ display: 'none' }}
-        />
-      </div>
-      <input
-        className={styles.nickNameInput}
-        type='text'
-        maxLength={16}
-        placeholder={userNickname}
-        value={editNickname}
-        onChange={handleNicknameChange}
-        onKeyDown={handleKeyDown}
-        required
-      />
-      <p className={styles.guideText}>닉네임은 공백 없이 16자 이하로 입력 가능합니다.</p>
-      <div className={styles.buttonsWrapper}>
-        <button className={styles.blueButton} type='submit'>
-          확인
-        </button>
-        <button className={styles.button} type='button' onClick={() => routeTo(-1)}>
-          취소
-        </button>
-      </div>
-    </form>
-  )
+  const props = {
+    routeTo,
+    userNickname,
+    editNickname,
+    previewImage,
+    handleImageChange,
+    handleDeleteImage,
+    handleNicknameChange,
+    handleKeyDown,
+    handleSubmit,
+  }
+
+  return <EditProfileView {...props} />
 }
 
 export default EditProfile
