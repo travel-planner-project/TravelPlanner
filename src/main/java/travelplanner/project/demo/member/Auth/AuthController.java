025facee3fda08d69bbf64060b089db1e703c72b
sieunnnn <wldsmtldsm65@gmail.com>
2023-08-03 18:19:43 +0900

    Test: 스웨거 문서 수정 (#120)

--- src/main/java/travelplanner/project/demo/member/Auth/AuthController.java
+++ src/main/java/travelplanner/project/demo/member/Auth/AuthController.java
@@ -13,7 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import travelplanner.project.demo.global.exception.ApiException;
 import travelplanner.project.demo.global.exception.ApiExceptionResponse;
 
 
