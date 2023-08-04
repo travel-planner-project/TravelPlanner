832e1febadeca4f4be5af96bd8b986123b476c34
sieunnnn <wldsmtldsm65@gmail.com>
2023-08-04 17:07:24 +0900

    Feat: 채팅 삭제 스케쥴러 작성 (#114)

--- src/main/java/travelplanner/project/demo/DemoApplication.java
+++ src/main/java/travelplanner/project/demo/DemoApplication.java
@@ -2,8 +2,9 @@ package travelplanner.project.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class DemoApplication {
 
