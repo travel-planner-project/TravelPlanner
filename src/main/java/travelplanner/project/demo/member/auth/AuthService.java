e8e0745ff2c0d8c061410c2732961c0e30b2da4d
sieunnnn <wldsmtldsm65@gmail.com>
2023-08-15 02:15:55 +0900

    Refac: 리프레시토큰 저장방식 변경 (# 130)

--- src/main/java/travelplanner/project/demo/member/auth/AuthService.java
+++ src/main/java/travelplanner/project/demo/member/auth/AuthService.java
@@ -90,16 +90,18 @@ public class AuthService {
 
         // 인증이 성공했을 때, 어세스 토큰과 리프레시 토큰 발급
         String accessToken = tokenUtil.generateAccessToken(member.getEmail());
-        String refreshToken = tokenUtil.generateRefreshToken(member.getEmail());
 
         // 어세스 토큰은 헤더에 담아서 응답으로 보냄
         response.setHeader("Authorization", accessToken);
 
-        // 리프레시 토큰은 쿠키에 담아서 응답으로 보냄
-        cookieUtil.create(refreshToken, response);
-
         // 리프레시 토큰을 Redis 에 저장
-        redisUtil.setData(member.getEmail(), refreshToken);
+        if (redisUtil.getData(member.getEmail()) == null) {
+
+            String refreshToken = tokenUtil.generateRefreshToken(member.getEmail());
+            // 리프레시 토큰은 쿠키에 담아서 응답으로 보냄
+            cookieUtil.create(refreshToken, response);
+            redisUtil.setData(member.getEmail(), refreshToken);
+        }
 
         return AuthResponse.builder()
                 .userId(member.getId())
