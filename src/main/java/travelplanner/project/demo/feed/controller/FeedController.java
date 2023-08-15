56e3a730f4472f0faeb3883310deb88455fc9f3b
toychip <dlawnsgud427@naver.com>
2023-08-15 14:05:12 +0900

    Feat: Feed 프로필 url추가 (#137)

--- src/main/java/travelplanner/project/demo/feed/controller/FeedController.java
+++ src/main/java/travelplanner/project/demo/feed/controller/FeedController.java
@@ -1,17 +1,13 @@
 package travelplanner.project.demo.feed.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import travelplanner.project.demo.feed.dto.FeedResponse;
 import travelplanner.project.demo.feed.service.FeedService;
 import travelplanner.project.demo.global.util.PageUtil;
-import travelplanner.project.demo.planner.domain.Planner;
-import travelplanner.project.demo.planner.dto.response.PlannerListResponse;
-import travelplanner.project.demo.planner.repository.PlannerRepository;
-
-import java.util.List;
 
 
 @RestController
@@ -25,7 +21,7 @@ public class FeedController {
             @RequestParam(required = false) String planTitle,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "6") int size){
-        PageUtil<FeedResponse> feedList = feedService.getFeedList(planTitle, page, size);
-        return null;
+        PageUtil<FeedResponse> feedList = feedService.getFeedList(planTitle, PageRequest.of(page, size));
+        return feedList;
     }
 }
--- src/main/java/travelplanner/project/demo/feed/service/FeedService.java
+++ src/main/java/travelplanner/project/demo/feed/service/FeedService.java
@@ -2,11 +2,17 @@ package travelplanner.project.demo.feed.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import travelplanner.project.demo.feed.dto.FeedResponse;
 import travelplanner.project.demo.global.util.PageUtil;
+import travelplanner.project.demo.planner.domain.Planner;
 import travelplanner.project.demo.planner.repository.PlannerRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -17,10 +23,34 @@ public class FeedService {
 
     private final PlannerRepository plannerRepository;
 
-    public PageUtil<FeedResponse> getFeedList(String planTitle, int page, int size) {
+    public PageUtil<FeedResponse> getFeedList(String planTitle, Pageable pageable) {
+        Page<Planner> page;
 
+        if (planTitle == null) {
+            // 전체 리스트를 가져오는 로직
+            page = plannerRepository.findAll(pageable);
+        } else {
+            // 제목으로 필터링된 결과를 가져오는 로직
+            page = plannerRepository.findByPlanTitleContaining(planTitle, pageable);
+        }
 
-        return null;
+        // Planner 객체를 FeedResponse 객체로 변환
+        List<FeedResponse> feedResponses = page.getContent()
+                .stream()
+                .map(planner -> new FeedResponse(
+                        planner.getId(),
+                        planner.getPlanTitle(),
+                        planner.getStartDate(),
+                        planner.getEndDate(),
+                        planner.getMember().getUserNickname(),
+                        planner.getMember().getProfile().getProfileImgUrl()
+                ))
+                .collect(Collectors.toList());
+
+        // PageUtil 객체 생성
+        PageUtil<FeedResponse> feedPage = new PageUtil<>(feedResponses, pageable, page.getTotalElements());
+
+        return feedPage;
     }
 
 }
