abe1c2d6d19776784eb4d8a7e76a9bc8ae23248f
iberis2 <snow9402@gmail.com>
2023-08-16 16:15:25 +0900

    Feat: Feed UI 완성 (#141)

--- client/src/components/Feed/PlannerTitle.tsx
+++ client/src/components/Feed/PlannerTitle.tsx
@@ -1,3 +1,44 @@
-export default function PlannerTitle() {
-  return <div>plannerTitle</div>
+import { differenceInDays, getMonth } from 'date-fns'
+import Icon from '../Common/Icon'
+import styles from './PlannerTitle.module.scss'
+
+type PlannerTitleProp = {
+  plannerId: number
+  planTitle: string
+  startDate: string
+  endDate: string
+  hostName: string
+  hostUrl: string
+}
+
+export default function PlannerTitle({
+  planTitle,
+  startDate,
+  endDate,
+  hostName,
+  hostUrl,
+}: PlannerTitleProp) {
+  const date = getMonth(new Date(startDate)) + 1
+  const dateDiffer = differenceInDays(new Date(endDate), new Date(startDate))
+
+  return (
+    <li className={styles.li}>
+      {hostUrl ? (
+        <img src={hostUrl} className={styles.img} alt='host profile' />
+      ) : (
+        <Icon name='profile' size={70} className={styles.profileIcon} />
+      )}
+      <div>
+        <h3 className={styles.title}>{planTitle}</h3>
+        <div className={styles.hostDateBox}>
+          <div className={styles.hostName}>{hostName}님의 계획</div>
+          <div className={styles.verticalLine} />
+          <div className={styles.date}>
+            {date}월의
+            {dateDiffer > 0 ? ` ${dateDiffer}박 ${dateDiffer + 1}일 여행` : ' 당일치기 여행'}
+          </div>
+        </div>
+      </div>
+    </li>
+  )
 }
--- client/src/pages/Feed.tsx
+++ client/src/pages/Feed.tsx
@@ -1,14 +1,57 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import PlannerTitle from '../components/Feed/PlannerTitle'
 import SearchBar from '../components/Feed/SearchBar'
 import styles from './Feed.module.scss'
 
+let data = [
+  {
+    plannerId: 1,
+    planTitle: '부산 여행 가기',
+    startDate: '2023-08-16T04:37:23.506Z',
+    endDate: '2023-08-17T04:37:23.506Z',
+    hostName: '준형',
+    hostUrl: '',
+    // 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4HbqZyTk4fRBYWt-7H6ubyM0ex6A8WyVunKD2mqOAmA&s',
+  },
+  {
+    plannerId: 2,
+    planTitle: '제주 여행 가기',
+    startDate: '2023-08-16T04:37:23.506Z',
+    endDate: '2023-08-16T04:37:23.506Z',
+    hostName: '준형',
+    hostUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4HbqZyTk4fRBYWt-7H6ubyM0ex6A8WyVunKD2mqOAmA&s',
+  },
+]
+
+type DatasType = {
+  plannerId: number
+  planTitle: string
+  startDate: string
+  endDate: string
+  hostName: string
+  hostUrl: string
+}[]
+
 export default function Feed() {
+  const [datas, setDatas] = useState<DatasType>([])
+
+  useEffect(() => {
+    setDatas(data)
+  }, [])
   return (
     <div className={styles.container}>
       <h1 className={styles.headline}>다른 사람들의 여행 계획을 둘러보세요!</h1>
       <div className={styles.line} />
       <SearchBar />
-      <PlannerTitle />
+      <ul>
+        {datas.map(data => (
+          <Link to={`/plandetail/${data.plannerId}`} key={data.plannerId}>
+            <PlannerTitle {...data} key={data.plannerId} />
+          </Link>
+        ))}
+      </ul>
     </div>
   )
 }
