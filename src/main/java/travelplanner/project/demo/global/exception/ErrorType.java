6ab04eafcdc4cf36cbdb659211c89f3db6cbdfce
seinShin <tlstpdls13@gmail.com>
2023-08-21 19:43:16 +0900

    Refac: 로그인 시 에러 분기 처리

--- src/main/java/travelplanner/project/demo/global/exception/ErrorType.java
+++ src/main/java/travelplanner/project/demo/global/exception/ErrorType.java
@@ -14,11 +14,14 @@ public enum ErrorType {
     // 권한이 부족한 경우
     USER_NOT_AUTHORIZED(HttpStatus.FORBIDDEN, "AUTH-001", "권한이 부족하여 접근할 수 없습니다."),
 
+    // 이메일이 일치하지 않은 경우
+    CHECK_EMAIL_AGAIN(HttpStatus.BAD_REQUEST, "USER-001", "이메일을 다시한번 확인해주세요"),
+
     // 비밀번호가 일치하지 않은 경우
-    CHECK_PASSWORD_AGAIN(HttpStatus.BAD_REQUEST, "USER-001", "비밀번호를 다시한번 확인해주세요"),
+    CHECK_PASSWORD_AGAIN(HttpStatus.BAD_REQUEST, "USER-002", "비밀번호를 다시한번 확인해주세요"),
 
     // 사용자를 찾을 수 없는 경우
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER-02", "유저가 존재하지 않습니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER-003", "유저가 존재하지 않습니다."),
 
     // 내부 서버 오류
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "SERVER-001", "내부 서버 오류로 인해 요청을 처리할 수 없습니다."),
@@ -36,7 +39,7 @@ public enum ErrorType {
 
 
     // 이미 이메일이 존재하는 경우
-    ALREADY_EXIST_EMAIL(HttpStatus.BAD_REQUEST, "USER-003", "이미 존재하는 이메일 입니다."),
+    ALREADY_EXIST_EMAIL(HttpStatus.BAD_REQUEST, "USER-004", "이미 존재하는 이메일 입니다."),
 
     PLANNER_NOT_AUTHORIZED(HttpStatus.FORBIDDEN, "PLANNER-001", "권한이 부족하여 접근할 수 없습니다."),
 
