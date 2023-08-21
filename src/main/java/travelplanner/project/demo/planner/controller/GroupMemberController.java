196a53e361999c61a665b0b18fcfa8de110a470a
seinShin <tlstpdls13@gmail.com>
2023-08-21 14:43:29 +0900

    Fix: 그룹멤버 검색 - RestAPI로 변경

--- src/main/java/travelplanner/project/demo/planner/controller/GroupMemberController.java
+++ src/main/java/travelplanner/project/demo/planner/controller/GroupMemberController.java
@@ -1,18 +1,23 @@
 package travelplanner.project.demo.planner.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import travelplanner.project.demo.global.util.TokenUtil;
 import travelplanner.project.demo.planner.dto.request.GroupMemberCreateRequest;
 import travelplanner.project.demo.planner.dto.request.GroupMemberDeleteRequest;
 import travelplanner.project.demo.planner.dto.request.GroupMemberSearchRequest;
+import travelplanner.project.demo.planner.dto.response.GroupMemberSearchResponse;
 import travelplanner.project.demo.planner.service.GroupMemberService;
 
+import java.util.Collections;
 import java.util.Map;
 
 @Controller
@@ -24,18 +29,15 @@ public class GroupMemberController {
     private final GroupMemberService groupMemberService;
     private final TokenUtil tokenUtil;
 
-    @MessageMapping("/search-member/{plannerId}")
-    public void searchGroupMember(
-            @DestinationVariable Long plannerId,
-            GroupMemberSearchRequest request,
-            @Header("Authorization") String authorization
-    ) throws Exception{
-
-        tokenUtil.getJWTTokenFromWebSocket(authorization);
+    @GetMapping("/search-member/{plannerId}")
+    @ResponseBody
+    public GroupMemberSearchResponse searchGroupMember( @PathVariable Long plannerId, @RequestBody GroupMemberSearchRequest request){
+        return groupMemberService.searchMember(plannerId, request);
+      /*  tokenUtil.getJWTTokenFromWebSocket(authorization);
         messagingTemplate.convertAndSend("/sub/planner-message/" + plannerId,
                 Map.of("type","search-user", "msg", groupMemberService.searchMember(request)
                 )
-        );
+        );*/
     }
 
     @MessageMapping("/add-member/{plannerId}")
@@ -62,6 +64,6 @@ public class GroupMemberController {
 
         tokenUtil.getJWTTokenFromWebSocket(authorization);
         groupMemberService.deleteGroupMember(request);
-        messagingTemplate.convertAndSend("/sub/planner-message/" + plannerId);
+        messagingTemplate.convertAndSend("/sub/planner-message/" + plannerId, Collections.emptyMap());
     }
 }
