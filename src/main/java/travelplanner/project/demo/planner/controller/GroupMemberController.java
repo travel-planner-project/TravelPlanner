d47360e9ecc884235addc2ca90e37e3d7cb24949
sieunnnn <wldsmtldsm65@gmail.com>
2023-08-24 17:22:32 +0900

    Docs: 스웨거 문서 추가

--- src/main/java/travelplanner/project/demo/planner/controller/GroupMemberController.java
+++ src/main/java/travelplanner/project/demo/planner/controller/GroupMemberController.java
@@ -1,5 +1,11 @@
 package travelplanner.project.demo.planner.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.Header;
@@ -10,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import travelplanner.project.demo.global.exception.ApiExceptionResponse;
 import travelplanner.project.demo.global.util.TokenUtil;
 import travelplanner.project.demo.planner.dto.request.GroupMemberCreateRequest;
 import travelplanner.project.demo.planner.dto.request.GroupMemberDeleteRequest;
@@ -20,6 +27,7 @@ import travelplanner.project.demo.planner.service.GroupMemberService;
 import java.util.Collections;
 import java.util.Map;
 
+@Tag(name = "Planner", description = "플래너 API")
 @Controller
 @RequiredArgsConstructor
 public class GroupMemberController {
@@ -29,13 +37,19 @@ public class GroupMemberController {
     private final GroupMemberService groupMemberService;
     private final TokenUtil tokenUtil;
 
+    @Operation(summary = "멤버 검색")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "그룹 멤버 검색 성공"),
+            @ApiResponse(responseCode = "404", description = "해당 이메일로 유저를 찾을 수 없는 경우",
+                    content = @Content(schema = @Schema(implementation = ApiExceptionResponse.class)))
+    })
     @GetMapping("/search/member")
     @ResponseBody
     public GroupMemberSearchResponse searchGroupMember(@RequestBody GroupMemberSearchRequest request){
         return groupMemberService.searchMember(request);
     }
 
-    @MessageMapping("/add-member/{plannerId}")
+    @MessageMapping("/add-m가ember/{plannerId}")
     public void addGroupMember(
             @DestinationVariable Long plannerId,
             GroupMemberCreateRequest request,
