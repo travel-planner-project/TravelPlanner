708533db576a85a9b268eb340f26a7cabc9981cc
iberis2 <snow9402@gmail.com>
2023-08-27 23:37:48 +0900

    formatting: useLogout에서 불필요한 async 삭제
    - useLogout 에서 api 로직 없어져서 async 삭제

--- client/src/hooks/useLogout.ts
+++ client/src/hooks/useLogout.ts
@@ -7,7 +7,7 @@ const useLogout = () => {
   const resetUserInfo = useResetRecoilState(userInfo)
   const { routeTo } = useRouter()
 
-  const logOut = async () => {
+  const logOut = () => {
     resetUserInfo()
     removeTokenFromSessionStorage()
     routeTo(0)
