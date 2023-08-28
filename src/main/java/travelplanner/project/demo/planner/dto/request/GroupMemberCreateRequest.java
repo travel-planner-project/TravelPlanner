f0547dbd09b34abe257a64f5ac40458ba3c36c89
toychip <dlawnsgud427@naver.com>
2023-08-28 12:52:48 +0900

    Refac: GroupMemberCreateRequest @Data -> @Builder 수정 (#171)

--- src/main/java/travelplanner/project/demo/planner/dto/request/GroupMemberCreateRequest.java
+++ src/main/java/travelplanner/project/demo/planner/dto/request/GroupMemberCreateRequest.java
@@ -1,12 +1,12 @@
 package travelplanner.project.demo.planner.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Schema(description = "그룹멤버 추가 요청 DTO")
-@Data
-@NoArgsConstructor
+@Getter
+@Builder
+@AllArgsConstructor
 public class GroupMemberCreateRequest {
 
     @Schema(description = "이메일", example = "user1@gmail.com")
