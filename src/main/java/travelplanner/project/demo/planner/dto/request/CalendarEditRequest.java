65cc5b12da4db0e72c77367555af853ce35fcfc4
toychip <dlawnsgud427@naver.com>
2023-08-28 09:23:12 +0900

    Refac: CalendarEditRequest @Data -> @builder 수정 (#171)

--- src/main/java/travelplanner/project/demo/planner/dto/request/CalendarEditRequest.java
+++ src/main/java/travelplanner/project/demo/planner/dto/request/CalendarEditRequest.java
@@ -1,15 +1,12 @@
 package travelplanner.project.demo.planner.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Schema(description = "날짜 수정 요청 DTO")
 @Getter
+@Builder
 @AllArgsConstructor
-
 public class CalendarEditRequest {
 
     @Schema(description = "날짜 인덱스", example = "1")
