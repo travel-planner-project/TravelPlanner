c05a1a24074a9c148f2732809d2cefc71348f6d6
toychip <dlawnsgud427@naver.com>
2023-09-08 01:21:10 +0900

    Feat: 유효하지 않은 에러 타입 추가 (#186)

--- src/main/java/travelplanner/project/demo/mail/controller/MailController.java
+++ src/main/java/travelplanner/project/demo/mail/controller/MailController.java
@@ -2,8 +2,12 @@ package travelplanner.project.demo.mail.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import travelplanner.project.demo.global.exception.ApiException;
+import travelplanner.project.demo.global.exception.ErrorType;
+import travelplanner.project.demo.global.util.AESUtil;
 import travelplanner.project.demo.global.util.TokenUtil;
 import travelplanner.project.demo.mail.dto.MailDto;
+import travelplanner.project.demo.mail.service.ForgotPasswordService;
 
 @RestController
 @RequestMapping("/forgot-password")
@@ -11,17 +15,24 @@ import travelplanner.project.demo.mail.dto.MailDto;
 public class MailController {
 
     private final TokenUtil tokenUtil;
+    private final ForgotPasswordService forgotPasswordService;
 
     @PostMapping
     public String getChangePasswordApi(@RequestBody MailDto mailDto) {
-
-
+        String tempToken = forgotPasswordService.generateTempToken(mailDto.getEmail(), mailDto.getNickName());
+        String resetLink = "https://yourwebsite.com/reset-password?token=" + tempToken;
 
         return null;
     }
 
     @GetMapping
-    public String getUriMailToken() {
+    public String getUriMailToken(@RequestParam String token) {
+
+        if (!tokenUtil.isValidToken(token)) {
+            throw new ApiException(ErrorType.TOKEN_NOT_VALID);
+        }
+
+
         return null;
     }
 
