129785cc471536803b6d644b3be15eacd9dedb4a
toychip <dlawnsgud427@naver.com>
2023-09-08 18:21:08 +0900

    Feat: MailService 초안 (#186)

--- src/main/java/travelplanner/project/demo/mail/service/MailService.java
+++ src/main/java/travelplanner/project/demo/mail/service/MailService.java
@@ -1,7 +1,21 @@
 package travelplanner.project.demo.mail.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MailService {
+
+    private final JavaMailSender mailSender;
+
+    public void sendSimpleMessage(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        mailSender.send(message);
+    }
 }
