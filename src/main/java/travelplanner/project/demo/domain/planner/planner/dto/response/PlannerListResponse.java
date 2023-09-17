f5add605852d3f0788c0bafa8735b7192bab418c
sieunnnn <wldsmtldsm65@gmail.com>
2023-09-17 13:14:53 +0900

    Feature: 리스폰스 수정 (#178)

--- src/main/java/travelplanner/project/demo/domain/planner/planner/dto/response/PlannerListResponse.java
+++ src/main/java/travelplanner/project/demo/domain/planner/planner/dto/response/PlannerListResponse.java
@@ -6,11 +6,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import travelplanner.project.demo.domain.planner.groupmember.dto.response.GroupMemberResponse;
 import travelplanner.project.demo.domain.planner.planner.domain.Planner;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Schema(description = "플래너 리스트 응답 DTO")
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
@@ -35,19 +35,22 @@ public class PlannerListResponse {
     @Schema(description = "여행 끝 날짜", example = "7/20")
     private LocalDateTime endDate;
 
+    private List<GroupMemberResponse> groupMembers;
+
     //플래너 객체
-    public PlannerListResponse(Planner planner){
+    public PlannerListResponse(Planner planner, List<GroupMemberResponse> groupMembers){
         this.plannerId = planner.getId();
         this.planTitle = planner.getPlanTitle();
         this.isPrivate = planner.getIsPrivate();
         this.startDate = planner.getStartDate();
         this.endDate = planner.getEndDate();
+        this.groupMembers = groupMembers;
     }
-    
-    //플래너 리스트
-    public static List<PlannerListResponse> plannerListResponse (List<Planner> entityList){
-        return entityList.stream().map(PlannerListResponse::new).collect(Collectors.toList());
-    }
+
+//    //플래너 리스트
+//    public static List<PlannerListResponse> plannerListResponse (List<Planner> entityList){
+//        return entityList.stream().map(PlannerListResponse::new).collect(Collectors.toList());
+//    }
 
     // 프로필 url 추가
 }
