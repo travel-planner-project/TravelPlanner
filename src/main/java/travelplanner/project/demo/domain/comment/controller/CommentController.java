e2ca599fd25e27491b90769d4d8dd7cd6e13c210
toychip <dlawnsgud427@naver.com>
2023-09-22 16:56:56 +0900

    Feat: Controller CommentList 조회 (#205)

--- src/main/java/travelplanner/project/demo/domain/comment/controller/CommentController.java
+++ src/main/java/travelplanner/project/demo/domain/comment/controller/CommentController.java
@@ -1,22 +1,31 @@
 package travelplanner.project.demo.domain.comment.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import travelplanner.project.demo.domain.comment.dto.request.CommentCreateRequest;
 import travelplanner.project.demo.domain.comment.dto.request.CommentDeleteRequest;
 import travelplanner.project.demo.domain.comment.dto.request.CommentEditRequest;
 import travelplanner.project.demo.domain.comment.dto.response.CommentResponse;
+import travelplanner.project.demo.domain.comment.service.CommentService;
 import travelplanner.project.demo.global.exception.ApiExceptionResponse;
 import travelplanner.project.demo.global.util.PageUtil;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 public class CommentController {
 
+    private final CommentService commentService;
 
     @Operation(summary = "댓글 리스트 조회")
     @ApiResponses(value = {
@@ -27,22 +36,27 @@ public class CommentController {
                     content = @Content(schema = @Schema(implementation = ApiExceptionResponse.class)))
     })
     @GetMapping
-    public PageUtil<CommentListResponse> getPostList() {
-        return null;
+    public List<CommentResponse> getPostList(
+
+            @Parameter(name = "postId", description = "게시글의 index", in = ParameterIn.QUERY)
+            @RequestParam(required = false) Long postId){
+
+        List<CommentResponse> commentList = commentService.getCommentList(postId);
+        return commentList;
     }
 
-    @Operation(summary = "댓글 상세 조회")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "댓글 상세 조회 성공"),
-            @ApiResponse(responseCode = "404", description = "댓글을 찾을 수 없습니다.",
-                    content = @Content(schema = @Schema(implementation = ApiExceptionResponse.class))),
-            @ApiResponse(responseCode = "404", description = "유저를 찾을 수 없습니다.",
-                    content = @Content(schema = @Schema(implementation = ApiExceptionResponse.class)))
-    })
-    @GetMapping("/{commentId}")
-    public CommentDetailResponse getCommentDetail(@PathVariable Long commentId) {
-        return null;
-    }
+//    @Operation(summary = "댓글 상세 조회")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "200", description = "댓글 상세 조회 성공"),
+//            @ApiResponse(responseCode = "404", description = "댓글을 찾을 수 없습니다.",
+//                    content = @Content(schema = @Schema(implementation = ApiExceptionResponse.class))),
+//            @ApiResponse(responseCode = "404", description = "유저를 찾을 수 없습니다.",
+//                    content = @Content(schema = @Schema(implementation = ApiExceptionResponse.class)))
+//    })
+//    @GetMapping("/{commentId}")
+//    public CommentResponse getCommentDetail(@PathVariable Long commentId) {
+//        return null;
+//    }
 
     @Operation(summary = "댓글 삭제")
     @ApiResponses(value = {
