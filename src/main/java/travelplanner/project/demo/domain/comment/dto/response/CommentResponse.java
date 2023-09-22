14d73748e3e2c519e7ba6d442ea4d470f70ec566
toychip <dlawnsgud427@naver.com>
2023-09-22 16:20:04 +0900

    Feat: createComment 추가 (#205)

--- src/main/java/travelplanner/project/demo/domain/comment/dto/response/CommentResponse.java
+++ src/main/java/travelplanner/project/demo/domain/comment/dto/response/CommentResponse.java
@@ -1,4 +1,17 @@
 package travelplanner.project.demo.domain.comment.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommentResponse {
+
+    private Long postId;
+    private Long commentId;
+    private String commentContent;
 }
--- src/main/java/travelplanner/project/demo/domain/comment/service/CommentService.java
+++ src/main/java/travelplanner/project/demo/domain/comment/service/CommentService.java
@@ -3,12 +3,17 @@ package travelplanner.project.demo.domain.comment.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import travelplanner.project.demo.domain.comment.domain.Comment;
 import travelplanner.project.demo.domain.comment.dto.request.CommentCreateRequest;
 import travelplanner.project.demo.domain.comment.dto.request.CommentEditRequest;
 import travelplanner.project.demo.domain.comment.dto.response.CommentResponse;
 import travelplanner.project.demo.domain.comment.dto.response.CommentDetailResponse;
 import travelplanner.project.demo.domain.comment.dto.response.CommentListResponse;
 import travelplanner.project.demo.domain.comment.repository.CommentRepository;
+import travelplanner.project.demo.domain.post.post.domain.Post;
+import travelplanner.project.demo.domain.post.post.repository.PostRepository;
+import travelplanner.project.demo.global.exception.ApiException;
+import travelplanner.project.demo.global.exception.ErrorType;
 
 import java.util.List;
 
@@ -17,6 +22,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CommentService {
 
+    private final PostRepository postRepository;
     private final CommentRepository commentRepository;
 
     public List<CommentListResponse> getCommentList(Long postId) {
@@ -27,8 +33,26 @@ public class CommentService {
         return null;
     }
 
+    @Transactional
     public CommentResponse createComment(Long postId, CommentCreateRequest commentCreateRequest) {
-        return null;
+
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new ApiException(ErrorType.POST_NOT_FOUND));
+
+        Comment comment = Comment.builder()
+                .post(post)
+                .commentContent(commentCreateRequest.getCommentContent())
+                .build();
+
+        commentRepository.save(comment);
+
+        CommentResponse response = CommentResponse.builder()
+                .commentId(comment.getId())
+                .postId(post.getId())
+                .commentContent(comment.getCommentContent())
+                .build();
+
+        return response;
     }
 
     @Transactional
