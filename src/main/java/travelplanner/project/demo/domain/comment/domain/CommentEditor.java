896d0fb9b1923a9fac5df5a1a38ab0bb746eca50
toychip <dlawnsgud427@naver.com>
2023-09-25 13:46:21 +0900

    Feat: CommentEditor 추가 (#205)

---
+++ src/main/java/travelplanner/project/demo/domain/comment/domain/CommentEditor.java
@@ -0,0 +1,40 @@
+package travelplanner.project.demo.domain.comment.domain;
+
+import lombok.Getter;
+
+@Getter
+public class CommentEditor {
+
+    private String commentContent;
+
+    public CommentEditor(String commentContent) {
+        this.commentContent = commentContent;
+    }
+
+    public static CommentEditorBuilder builder() {
+        return new CommentEditorBuilder();
+    }
+
+    public static class CommentEditorBuilder{
+        private String commentContent;
+
+        CommentEditorBuilder() {
+        }
+
+        public CommentEditorBuilder commentContent(final String commentContent) {
+            if (commentContent != null && commentContent.isEmpty()) {
+                this.commentContent = commentContent;
+            }
+            return this;
+        }
+
+        public CommentEditor build() {
+            return new CommentEditor(commentContent);
+        }
+    }
+
+    public String toString() {
+        return "CommentEditor.CommentEditorBuilder(commentContent=" + this.commentContent + ")";
+    }
+
+}
