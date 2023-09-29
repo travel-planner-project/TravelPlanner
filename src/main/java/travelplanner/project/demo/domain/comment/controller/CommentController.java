018494b5987d16197f1cfcc1081d05262a396682
toychip <dlawnsgud427@naver.com>
2023-09-30 00:09:24 +0900

    Docs: CommentController Swagger 추가 (#205)

--- src/main/java/travelplanner/project/demo/domain/comment/controller/CommentController.java
+++ src/main/java/travelplanner/project/demo/domain/comment/controller/CommentController.java
@@ -27,25 +27,19 @@ public class CommentController {
 
     private final CommentService commentService;
 
-    @Operation(summary = "댓글 리스트 조회")
+    // 댓글 리스트 조회
+    @Operation(summary = "댓글 리스트 조회", description = "특정 게시글의 댓글 목록을 페이지 단위로 조회합니다.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "댓글 리스트 조회 성공"),
-            @ApiResponse(responseCode = "404", description = "댓글을 찾을 수 없습니다.",
-                    content = @Content(schema = @Schema(implementation = ApiExceptionResponse.class))),
-            @ApiResponse(responseCode = "404", description = "댓글 찾을 수 없습니다.",
+            @ApiResponse(responseCode = "200", description = "댓글 리스트 조회 성공",
+                    content = @Content(schema = @Schema(implementation = PageUtil.class))),
+            @ApiResponse(responseCode = "404", description = "댓글이나 게시글을 찾을 수 없습니다.",
                     content = @Content(schema = @Schema(implementation = ApiExceptionResponse.class)))
     })
     @GetMapping
     public PageUtil<CommentResponse> getCommentList(
-            @Parameter(name="page", description = "몇번째 페이지(0부터 시작), 기본값 0", in = ParameterIn.QUERY)
-            @RequestParam(defaultValue = "0") int page,
-
-            @Parameter(name = "size", description = "희망 플래너 갯수, 기본값 6", in = ParameterIn.QUERY) // swagger
-            @RequestParam(defaultValue = "6") int size,
-
-            @Parameter(name = "postId", description = "게시글의 index", in = ParameterIn.QUERY)
-            @RequestParam(required = false) Long postId){
-
+            @Parameter(description = "페이지 번호(0부터 시작), 기본값 0") @RequestParam(defaultValue = "0") int page,
+            @Parameter(description = "페이지 크기, 기본값 6") @RequestParam(defaultValue = "6") int size,
+            @Parameter(description = "게시글 ID") @RequestParam(required = false) Long postId) {
 
         PageUtil<CommentResponse> commentList = commentService.getCommentList(postId, PageRequest.of(page, size));
 
@@ -65,23 +59,26 @@ public class CommentController {
 //        return null;
 //    }
 
-    @Operation(summary = "댓글 삭제")
+    @Operation(summary = "댓글 삭제", description = "특정 게시글의 특정 댓글을 삭제합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "댓글 삭제 성공"),
             @ApiResponse(responseCode = "403", description = "권한이 부족하여 접근할 수 없습니다.",
                     content = @Content(schema = @Schema(implementation = ApiExceptionResponse.class))),
             @ApiResponse(responseCode = "404", description = "댓글 찾을 수 없습니다.",
+                    content = @Content(schema = @Schema(implementation = ApiExceptionResponse.class))),
+            @ApiResponse(responseCode = "500", description = "내부 서버 오류로 인해 요청을 처리할 수 없습니다.",
                     content = @Content(schema = @Schema(implementation = ApiExceptionResponse.class)))
     })
     @DeleteMapping("/{postId}/{commentId}")
-    //포스트 삭제
     public void deleteComment(@PathVariable Long postId, @PathVariable Long commentId, HttpServletRequest request) {
         commentService.deleteComment(postId, commentId, request);
     }
 
-    @Operation(summary = "댓글 생성")
+    @Operation(summary = "댓글 생성", description = "특정 게시글에 새로운 댓글을 추가합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "댓글 생성 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 입력 값",
+                    content = @Content(schema = @Schema(implementation = ApiExceptionResponse.class))),
             @ApiResponse(responseCode = "500", description = "입력하지 않은 요소가 존재합니다.",
                     content = @Content(schema = @Schema(implementation = ApiExceptionResponse.class)))
     })
@@ -94,19 +91,23 @@ public class CommentController {
     }
 
 
-    @Operation(summary = "댓글 수정")
+    @Operation(summary = "댓글 수정", description = "댓글의 내용을 수정합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "댓글 수정 성공"),
-            @ApiResponse(responseCode = "404", description = "댓글을 찾을 수 없습니다.",
+            @ApiResponse(responseCode = "400", description = "잘못된 요청 파라미터",
                     content = @Content(schema = @Schema(implementation = ApiExceptionResponse.class))),
-            @ApiResponse(responseCode = "500", description = "입력하지 않은 요소가 존재합니다.",
+            @ApiResponse(responseCode = "404", description = "댓글이나 게시글을 찾을 수 없습니다.",
+                    content = @Content(schema = @Schema(implementation = ApiExceptionResponse.class))),
+            @ApiResponse(responseCode = "403", description = "권한이 부족하여 접근할 수 없습니다.",
                     content = @Content(schema = @Schema(implementation = ApiExceptionResponse.class)))
     })
     @PatchMapping("/{postId}/{commentId}")
-    public ResponseEntity updateComment(@PathVariable Long postId, @PathVariable Long commentId,
-                                        @RequestBody CommentEditRequest commentEditRequest,
-                                        HttpServletRequest request) {
-
+    public ResponseEntity updateComment(
+            @Parameter(description = "게시글 ID", required = true) @PathVariable Long postId,
+            @Parameter(description = "댓글 ID", required = true) @PathVariable Long commentId,
+            @Parameter(description = "수정할 댓글 정보", required = true) @RequestBody CommentEditRequest commentEditRequest,
+            HttpServletRequest request
+    ) {
         commentService.editComment(postId, commentId, commentEditRequest, request);
 
         return ResponseEntity.ok().body("댓글 수정 성공");
